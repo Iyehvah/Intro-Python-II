a1b76a5170f9a48007fe3487264becb609f3565b
Iyehvah <Iyehvah@users.noreply.github.com>
2020-06-09 20:53:50 -0500

    Will figure out how to add a room to the player tomorrow

--- src/player.py
+++ src/player.py
@@ -1,2 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player:
+    def __init__(self, player, room):
+        self.player = player
+        self.room = room
+    
+    def __str__(self):
+        output = f"This is player {self.player}"
+
+    def __repr__(self):
+        return f"self.player = {self.player}"
+
+new_player = Player("Bob")
+
+print(new_player)
